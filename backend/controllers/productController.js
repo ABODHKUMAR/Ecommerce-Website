f176e39649fb4ab75f33c2d005269889def75b52
Abodh <abodh5921@gmail.com>
2023-10-08 14:18:41 +0530

    Add updateProduct deleteProduct getProductDetails function API

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,4 @@
-const productModel = require("../models/productModel");
+// const productModel = require("../models/productModel");
 const Product = require("../models/productModel");
 
 
@@ -25,4 +25,75 @@ exports.getAllProducts = async (req, res) => {
         sucess:true,
         products
      });
+}
+
+//Update Product -- Admin
+
+exports.updateProduct =async (req,res)=>{
+   let product =await Product.findById(req.params.id);
+   if(!product)
+   {
+      return res.status(500).json({
+         sucess:false ,
+         message : "Product not found" ,
+      })
+   }
+   console.log(product);
+   product = await Product.findByIdAndUpdate(req.params.id,req.body,{
+      new : true,
+      runValidators:true,
+      useFindAndModify:false
+   });
+  console.log(product);
+   res.status(200).json({
+      sucess:true,
+      product
+   })
+
+
+}
+
+//Delete Product
+
+exports.deleteProduct = async(req,res,next)=>
+{
+   const product = await Product.findById(req.params.id);
+
+   if(!product)
+   {
+      return res.status(500).json({
+         success : false ,
+         message: "Product not found" ,
+      })
+   }
+
+   await product.deleteOne({ _id: req.params.id });
+
+   res.status(200).json({
+      success:true,
+      message:"Product Deleted Sucessfully",
+   })
+
+}
+
+// Get Products Details
+
+exports.getProductDetails = async(req,res,next)=>
+{
+   const product = await Product.findById(req.params.id);
+
+   if(!product)
+   {
+      return res.status(500).json({
+         success : false ,
+         message: "Product not found" ,
+      })
+   }
+
+
+   res.status(200).json({
+      success:true,
+      product
+   })
+
 }
